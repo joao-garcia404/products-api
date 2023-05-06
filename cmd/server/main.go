c878466d6ea6d46a6044a039fd54e035d6e32d51
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2023-05-06 11:37:05 -0300

    Feat: Implementing product handlers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,44 @@
 package main
 
-import "github.com/joao-garcia404/products-api/configs"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joao-garcia404/products-api/configs"
+	"github.com/joao-garcia404/products-api/internal/entity"
+	"github.com/joao-garcia404/products-api/internal/infra/database"
+	"github.com/joao-garcia404/products-api/internal/infra/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
-	config, _ := configs.LoadConfig(".")
+	_, err := configs.LoadConfig(".")
 
-	println(config.DBUser)
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(entity.Product{}, &entity.User{})
+
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/products", productHandler.GetProducts)
+	r.Get("/products/{id}", productHandler.GetProduct)
+	r.Post("/products", productHandler.CreateProduct)
+	r.Put("/products/{id}", productHandler.UpdateProduct)
+	r.Delete("/products/{id}", productHandler.DeleteProduct)
+
+	http.ListenAndServe(":8000", r)
 }
