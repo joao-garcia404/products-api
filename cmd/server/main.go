90769e038a319f98f2c31eb3260a6f5f329eb356
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2023-05-07 00:26:02 -0300

    Feat: Implementing user handlers and protecting products routs

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"net/http"
 
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/jwtauth"
 	"github.com/joao-garcia404/products-api/configs"
 	"github.com/joao-garcia404/products-api/internal/entity"
 	"github.com/joao-garcia404/products-api/internal/infra/database"
@@ -14,7 +15,7 @@ import (
 )
 
 func main() {
-	_, err := configs.LoadConfig(".")
+	configs, err := configs.LoadConfig(".")
 
 	if err != nil {
 		panic(err)
@@ -29,16 +30,32 @@ func main() {
 	db.AutoMigrate(entity.Product{}, &entity.User{})
 
 	productDB := database.NewProduct(db)
+	userDB := database.NewUser(db)
+
 	productHandler := handlers.NewProductHandler(productDB)
+	userHandler := handlers.NewUserHandler(userDB)
 
 	r := chi.NewRouter()
-	r.Use(middleware.Logger)
 
-	r.Get("/products", productHandler.GetProducts)
-	r.Get("/products/{id}", productHandler.GetProduct)
-	r.Post("/products", productHandler.CreateProduct)
-	r.Put("/products/{id}", productHandler.UpdateProduct)
-	r.Delete("/products/{id}", productHandler.DeleteProduct)
+	r.Use(middleware.Logger, middleware.Recoverer)
+	r.Use(middleware.WithValue("jwt", configs.TokenAuth))
+	r.Use(middleware.WithValue("JwtExpiresIn", configs.JWTExpiresIn))
+
+	r.Route("/products", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(configs.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+
+		r.Get("/", productHandler.GetProducts)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Post("/", productHandler.CreateProduct)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+	})
+
+	r.Route("/users", func(r chi.Router) {
+		r.Post("/", userHandler.CreateUser)
+		r.Post("/generate_token", userHandler.GetJWT)
+	})
 
 	http.ListenAndServe(":8000", r)
 }
